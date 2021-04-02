be7af339258cfa1baa664e6b6c6bd299a3b34312
Joon Won Park <jpark9493@gmail.com>
2021-04-02 11:17:40 -0400

    add lodash and resolve function

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,6 +3,13 @@ const graphql = require('grpahql');
 
 const { GraphQLObjectType, GrpahQLString } = graphql;
 
+// temp test data
+var books = [
+    {name: 'Name of the Wind', genre:'Fantasy', id:'1'},
+    {name: 'The Final Empire', genre:'Fantasy', id:'2'},
+    {name: 'The Long Earth', genre:'Sci-Fi', id:'3'},
+];
+
 const BookType = new GraphQLObjectType({
     name: 'Book',
     // making fields as a function helps you with references 
@@ -23,6 +30,7 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GrpahQlString }},
             resolve(parent, args){
                 // code to get data from db / other source
+                return _.find(books, {id: args.id});
             }
         } 
     }
