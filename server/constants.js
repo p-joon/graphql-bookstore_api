20e6fe11b738cbf55fc773af562d16b1d83585fa
Joon Won Park <jpark9493@gmail.com>
2021-04-02 16:22:17 -0400

    add models and mutations

--- server/constants.js
+++ server/constants.js
@@ -1,3 +1,4 @@
 module.exports = Object.freeze( {
-    DB_LINK: 'PASTE YOUR DB LINK HERE'
+    DB_LINK: 'mongodb+srv://test-user:qwe123@cluster0.w8yr4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    // DB_LINK: 'PASTE YOUR DB LINK HERE'
 });
