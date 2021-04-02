5451c950cd14a3146bc69cdfc91153bdbf82653d
Joon Won Park <jpark9493@gmail.com>
2021-04-02 12:09:42 -0400

    add type relations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -11,9 +11,9 @@ const {
 
 // temp test data
 var books = [
-    { name: 'Name of the Wind', genre:'Fantasy', id:'1' },
-    { name: 'The Final Empire', genre:'Fantasy', id:'2' },
-    { name: 'The Long Earth', genre:'Sci-Fi', id:'3' },
+    { name: 'Name of the Wind', genre:'Fantasy', id:'1', authorId: '1' },
+    { name: 'The Final Empire', genre:'Fantasy', id:'2', authorId: '2' },
+    { name: 'The Long Earth', genre:'Sci-Fi', id:'3', authorId: '3' }
 ];
 
 var authors = [
@@ -29,7 +29,13 @@ const BookType = new GraphQLObjectType({
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
-        genre: { type: GraphQLString }
+        genre: { type: GraphQLString },
+        author: {
+            type: AuthorType,
+            resolve(parent, args) {
+                return _.find(authors, { id: parent.authorId });
+            }
+        }
     })
 });
 
@@ -50,7 +56,7 @@ const RootQuery = new GraphQLObjectType({
         book: {
             type: BookType,
             args: { id: { type: GraphQLID }},
-            resolve(parent, args){
+            resolve(parent, args) {
                 // code to get data from db / other source
                 return _.find(books, { id: args.id });
             }
